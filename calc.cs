833fe8b4beed80bf3df70da513a2ec3a08182802
Rutvij Kimmatkar <101335151+RutvijKimmatkar@users.noreply.github.com>
2024-08-07 01:47:21 +0530

    v1
    added addition and subtraction

---
+++ calc.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void add(int num1, int num2)
+    {
+        Console.WriteLine("Answer is: "+ (num1 + num2));
+    }
+    static void sub(int num1, int num2)
+    {
+
+        Console.WriteLine(num1 - num2);
+    }
+    static void Main()
+    {
+        Console.WriteLine("select operation, 1.Addition 2.Subtraction");
+        int operation = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter first number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter second number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        if (operation == 1)
+        {
+            add(num1, num2);
+        }
+        else if (operation == 2)
+        {
+            sub(num1, num2);
+        }
+        else
+        {
+            Console.WriteLine("invalid operation selected");
+        }
+
+    }
+}
