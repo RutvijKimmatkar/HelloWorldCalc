3209b3df5a71d65ce236996088b4b932180aca2e
Rutvij Kimmatkar <101335151+RutvijKimmatkar@users.noreply.github.com>
2024-10-30 13:40:38 +0530

    Update calc.cs
    Calc v0.4

--- calc.cs
+++ calc.cs
@@ -1,89 +1,169 @@
-﻿class Program
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+class Program
 {
-    static int add(int num1, int num2)
+    
+    static float add(float num1, float num2)
     {
         Console.Write(num1 + "+" + num2 + "=");
         return num1 + num2;
     }
-    static int sub(int num1, int num2)
+    static float sub(float num1, float num2)
     {
         Console.Write(num1 + "-" + num2 + "=");
         return num1 - num2;
     }
-    static int division(int num1, int num2)
+    static float division(float num1, float num2)
     {
-        if (num2 == 0)
+        while(num2==0)
         {
-            Console.WriteLine("Division is not possible by zero.");
-            return 0; //this line is important else we are letting the calculation continue
+            Console.WriteLine("division is not possible by zero");
+            Console.WriteLine("enter new second number");
+            num2=Convert.ToInt32(Console.ReadLine());
+        
         }
+        
+
+        
+        
+        
         Console.Write(num1 + "/" + num2 + "=");
         return num1 / num2;
     }
-    static int multi(int num1, int num2)
+    static float multi(float num1, float num2)
     {
         Console.Write(num1 + "x" + num2 + "=");
         return num1 * num2;
     }
 
     //backup
-
+    
     static void Main()
     {
-
+        List<string> history= new List<string>();
+        int historyindex=0;
+        bool userchoice = true;
+        string operationsign="na";
+        while(userchoice)
+        {
+        Console.Clear();
         Console.WriteLine("Welcome to Calculator. Select an operation.");
-        Console.WriteLine("1. Addition 2.Subtraction 3.Division 4.Multiplication");
-        bool validation = (!int.TryParse(Console.ReadLine(), out int operation)) || operation < 1 || operation > 4; // if true, input is stored in int operation(aka output or out)
+        Console.WriteLine("1.Addition\n2.Subtraction\n3.Division\n4.Multiplication\n5.History\n6.Exit\n7.Factorial");
+        bool validation = (!float.TryParse(Console.ReadLine(), out float operation)) || operation < 1 || operation > 7; // if true, input is stored in float operation(aka output or out)
         // || is OR operator, ! is negation.
         // if any of the edge cases is true,
         // validation will take place, else not. all need not be true, only one is sufficient.
-        // int.tryparse converts string to int,
+        // float.tryparse converts string to float,
         // if successful returns true, if not returns false.
         // we used negation here for tryparse. we expect a number, so when we receive a number we negate it to give way to other checks on the right.
         if (validation)
         {
             Console.WriteLine("Please enter a valid number.");
-            return; //this line is important, else program continues. we are in a void so dont put return 0;
+            Console.ReadKey();
+            continue; //this line is important, else program continues. we are in a void so dont put return 0;
         }
+        if(operation==5)
+        {
+            if(history.Count==0)
+            {
+                Console.WriteLine("history is empty, press any key to continue");
+                Console.ReadLine();
+            }
+            else
+            foreach(string n in history)
+            {
+                Console.WriteLine(n);
+
+                
+            }
+            Console.WriteLine("press any key to continue");
+            Console.ReadLine();            
+            continue;
+        }
+        if(operation==6)
+        {
+            Console.WriteLine("Thank you!");
+            break;
+        }
+        if(operation==7)
+        {
+            Console.WriteLine("Selected Factorial");
+            Console.WriteLine("enter number");
+            int fact=Convert.ToInt32(Console.ReadLine());
+            int result1=1;
+            for(int i =1; i<=fact; i++)
+            {
+               result1=result1*i;
+            }
+            Console.WriteLine(result1);
+            string expression =(fact+"!"+"="+result1);
+            history.Add(expression);
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+            continue;
+        }                   
         Console.WriteLine("enter first number");
-        if ((!int.TryParse(Console.ReadLine(), out int num1)))
+        if ((!float.TryParse(Console.ReadLine(), out float num1)))
         {
             Console.WriteLine("Enter a valid number.");
             return;
         }
         Console.WriteLine("enter second number");
-        if ((!int.TryParse(Console.ReadLine(), out int num2)))
+        if ((!float.TryParse(Console.ReadLine(), out float num2)))
         {
             Console.WriteLine("Enter a valid number.");
             return;
         }
-        int result = 0;
+
+        float result = 0;
         switch (operation)
         {
             case 1:
                 Console.WriteLine("Selected Addition");
-                int sum = add(num1, num2);
+                float sum = add(num1, num2);
                 result = sum;
+                operationsign="+";
                 break;
             case 2:
                 Console.WriteLine("Selected Subtraction");
-                int diff = sub(num1, num2);
+                float diff = sub(num1, num2);
                 result = diff;
+                operationsign="-";
                 break;
             case 3:
                 Console.WriteLine("Selected Division");
-                int div = division(num1, num2);
+                float div = division(num1, num2);
                 result = div;
+                operationsign="/";
                 break;
             case 4:
                 Console.WriteLine("Selected Multiplication");
-                int product = multi(num1, num2);
+                float product = multi(num1, num2);
                 result = product;
+                operationsign="x";
                 break;
+            case 7:
+                
+            
             default:
                 Console.WriteLine("Enter a valid selection");
                 break;
         }
         Console.Write(result); //reduced redunduncy
+        string calculation =(num1+operationsign+num2+"="+result);
+        history.Add(calculation);
+        Console.WriteLine("\nWould you like to continue? Enter 'exit' to quit.");
+        if((Console.ReadLine()=="exit"))
+        {
+            userchoice=false;
+            Console.WriteLine("Thank you!");
+
+        }
+
+        }
+
     }
-}
+    }
+
